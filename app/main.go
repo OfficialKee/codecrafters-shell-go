079728e260899bf1912893b3a361af3567513282
officialkee <shikee.maloney@outlook.com>
2025-10-07 15:47:04 -0400

    [any message]

--- app/main.go
+++ app/main.go
@@ -7,13 +7,18 @@ import (
 )
 
 func main() {
-	// Uncomment this block to pass the first stage
-	fmt.Fprint(os.Stdout, "$ ")
 
-	// Wait for user input
-	command, err := bufio.NewReader(os.Stdin).ReadString('\n')
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	//repl := true
+	// Uncomment this block to pass the first stage
+	//fmt.Fprint(os.Stdout, "$ ")
+
+	//// Wait for user input
+	for {
+		fmt.Fprint(os.Stdout, "$ ")
+		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+		fmt.Println(command[:len(command)-1] + ": command not found")
 	}
-	fmt.Println(command[:len(command)-1] + ": command not found")
 }
